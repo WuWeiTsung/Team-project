90b04930621094fceca22c1dc687dda58e0b2b0a
RitaYang <ritayang0811@gmail.com>
2023-08-19 02:51:11 +0800

    finish navbar collapse & change icon color

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 console.log("Hello world!");
+document.addEventListener(
+  "click",
+
+  function (event) {
+    // 获取点击的元素
+    const clickedElement = event.target;
+
+    // 获取要关闭的折叠元素
+    const collapseElement = document.querySelector(".collapse");
+    const collapseOverlay = document.querySelector(".collapse-overlay");
+
+    // 判断点击的元素是否为遮罩层
+    if (clickedElement === collapseOverlay) {
+      // 关闭折叠
+      const collapse = new bootstrap.Collapse(collapseElement, {
+        toggle: false,
+      });
+      collapse.hide();
+    }
+  }
+);
